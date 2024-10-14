d7992f171837cfd7dd4a9d8b7db53f19550964aa
chiscookeke11 <chiscookeke11@gmail.com>
2024-10-13 19:52:21 -1100

    added the mont select elemnt

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <form action="">
 
           <div className="title">
-            <img src="" alt="" />
+            <img src="" alt="Logo" />
             <h2>Flow Insights</h2>
             <p>Imagine uncovering the truth about the worlds behaviour with minimal error and maximum privacy</p>
           </div>
@@ -28,8 +28,22 @@ function App() {
 
           <div className="name-input">
             <input type="number" id="day" name="day" min="1" max="31" placeholder="Day"/>
-            <input type="month" placeholder='Address' required/>
-            <input type="month" id="month" name="month"/>
+            <select name="month" id="">
+              <option value="">Select Month</option>
+              <option value="">January</option>
+              <option value="">Febuary</option>
+              <option value="">March</option>
+              <option value="">April</option>
+              <option value="">May</option>
+              <option value="">June</option>
+              <option value="">July</option>
+              <option value="">August</option>
+              <option value="">September</option>
+              <option value="">October</option>
+              <option value="">November</option>
+              <option value="">December</option>
+            </select>
+            <input type="number" id="year" name="year" min="1900" max="2024" />
           </div>
         </form>
         </div>
